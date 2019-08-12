37add9ee5446e7243d90b3cb335019a5c523d725
hoan.lee <lonelywolf.lee@gmail.com>
2019-08-12 11:50:17 +0700

    Setup project for kakaopay coding test

---
+++ src/main/kotlin/com/kakaopay/codingtest/KakaopayCodingTest2019Application.kt
@@ -0,0 +1,11 @@
+package com.kakaopay.codingtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KakaopayCodingTest2019Application
+
+fun main(args: Array<String>) {
+    runApplication<KakaopayCodingTest2019Application>(*args)
+}

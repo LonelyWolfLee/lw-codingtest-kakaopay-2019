fe5fc52645ae5bab90b1cac2bd28c00ff49526e3
hoan.lee <lonelywolf.lee@gmail.com>
2019-08-13 10:01:48 +0900

    Update test for auth

--- src/test/kotlin/com/kakaopay/codingtest/IntegrationTests.kt
+++ src/test/kotlin/com/kakaopay/codingtest/IntegrationTests.kt
@@ -5,18 +5,36 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.http.HttpStatus
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+class IntegrationTests(
+        @Autowired val restTemplate: TestRestTemplate,
+        @Autowired val authService: AuthService
+) {
 
     @Test
-    fun `Assert api environment is set up`() {
-        val entity = restTemplate.getForEntity<String>("/api/info")
+    fun `Auth signup can verified`() {
+        val userId = "test id1"
+        val password = "0000"
+
+        val entity = restTemplate.postForEntity<AuthToken>("/api/auth/signup", Account(userId, password))
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).isEqualTo("Kakaopay Coding Test 2019")
+        val authToken = entity.body
+        assertThat(authService.verify(authToken!!.token)!!.userId).isEqualTo(userId)
+    }
+
+    @Test
+    fun `Auth can signin after signup`() {
+        val userId = "test id2"
+        val password = "0000"
+
+        val entity = restTemplate.postForEntity<AuthToken>("/api/auth/signup", Account(userId, password))
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        val entity2 = restTemplate.postForEntity<AuthToken>("/api/auth/signin", Account(userId, password))
+        assertThat(entity2.statusCode).isEqualTo(HttpStatus.OK)
     }
 
 }
